fe6883ea1b11fe967800a5df6ec2da14d532efc7
Omar Khan <oakhan3@gmail.com>
2019-03-21 11:36:10 -0400

    Nest psycopg2 imports in patch function to prevent import errors for non-postgres users.

--- src/pytest_mock_resources/patch/redshift/psycopg2_connect.py
+++ src/pytest_mock_resources/patch/redshift/psycopg2_connect.py
@@ -4,7 +4,6 @@ except ImportError:
     from mock import patch  # type: ignore
 
 from decorator import decorator
-from psycopg2 import connect, extensions
 from sqlalchemy import create_engine
 from sqlalchemy.sql.elements import TextClause
 
@@ -14,27 +13,6 @@ from pytest_mock_resources.patch.redshift.create_engine import (
     execute_mock_s3_unload_command,
 )
 from pytest_mock_resources.patch.redshift.mock_s3_copy import strip
-
-
-class CustomCursor(extensions.cursor):
-    """A custom cursor class to define a custom execute method."""
-
-    def execute(self, sql, args=None):
-        dsn_params = self.connection.get_dsn_parameters()
-        engine = create_engine(
-            "postgresql://{user}:{password}@{hostname}:{port}/{dbname}".format(
-                user=dsn_params["user"],
-                password=config["password"],
-                hostname=dsn_params["host"],
-                port=dsn_params["port"],
-                dbname=dsn_params["dbname"],
-            )
-        )
-        if not isinstance(sql, TextClause) and strip(sql).lower().startswith("copy"):
-            return execute_mock_s3_copy_command(sql, engine)
-        if not isinstance(sql, TextClause) and strip(sql).lower().startswith("unload"):
-            return execute_mock_s3_unload_command(sql, engine)
-        return extensions.cursor.execute(self, sql, args)
 
 
 @decorator
@@ -49,5 +27,27 @@ def patch_psycopg2_connect(func, path=None, *args, **kwargs):
 
 def mock_psycopg2_connect(*args, **kwargs):
     """Substitute the default cursor with a custom cursor."""
+    from psycopg2 import connect, extensions  # noqa
+
+    class CustomCursor(extensions.cursor):
+        """A custom cursor class to define a custom execute method."""
+
+        def execute(self, sql, args=None):
+            dsn_params = self.connection.get_dsn_parameters()
+            engine = create_engine(
+                "postgresql://{user}:{password}@{hostname}:{port}/{dbname}".format(
+                    user=dsn_params["user"],
+                    password=config["password"],
+                    hostname=dsn_params["host"],
+                    port=dsn_params["port"],
+                    dbname=dsn_params["dbname"],
+                )
+            )
+            if not isinstance(sql, TextClause) and strip(sql).lower().startswith("copy"):
+                return execute_mock_s3_copy_command(sql, engine)
+            if not isinstance(sql, TextClause) and strip(sql).lower().startswith("unload"):
+                return execute_mock_s3_unload_command(sql, engine)
+            return extensions.cursor.execute(self, sql, args)
+
     conn = connect(cursor_factory=CustomCursor, *args, **kwargs)
     return conn
