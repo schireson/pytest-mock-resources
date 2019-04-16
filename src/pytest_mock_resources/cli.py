1625276a80c53918c1274431ce4481232f0ed51a
Omar Khan <oakhan3@gmail.com>
2019-04-16 12:11:35 -0400

    Create PMR CLI for reducing fixture startup lag by pre-emptively upping docker containers. Github issue #40.

---
+++ src/pytest_mock_resources/cli.py
@@ -0,0 +1,73 @@
+import argparse
+import enum
+import subprocess  # nosec
+
+
+@enum.unique
+class FixtureBase(enum.Enum):
+    POSTGRES = "postgres"
+    MONGO = "mongo"
+
+    @classmethod
+    def options(cls):
+        return ", ".join(fixture_base.value for fixture_base in cls)
+
+    @property
+    def command(self):
+        postgres_command = [
+            "docker",
+            "run",
+            "-d",
+            "-p",
+            "5532:5432",
+            "-e",
+            "POSTGRES_DB=dev",
+            "-e",
+            "POSTGRES_USER=user",
+            "-e",
+            "POSTGRES_PASSWORD=password",
+            "postgres:9.6.10-alpine",
+        ]
+        mongo_command = [
+            "docker",
+            "run",
+            "-d",
+            "-p",
+            "28017:27017",
+            "-e",
+            "MONGO_INITDB_ROOT_USERNAME=user",
+            "-e",
+            "MONGO_INITDB_ROOT_PASSWORD=password",
+            "mongo:3.6",
+        ]
+        fixture_base_command_map = {
+            FixtureBase.MONGO: mongo_command,
+            FixtureBase.POSTGRES: postgres_command,
+        }
+
+        return fixture_base_command_map[self]
+
+
+# TODO: Add an options arg to DOWN a fixture_base's container
+parser = argparse.ArgumentParser(
+    description="Premptively run docker containers to speed up initial startup of PMR Fixtures."
+)
+parser.add_argument(
+    "fixture_bases",
+    metavar="Fixture Base",
+    type=str,
+    nargs="+",
+    help="Available Fixture Bases: {}".format(FixtureBase.options()),
+)
+
+
+def main():
+    args = parser.parse_args()
+
+    for fixture_base in args.fixture_bases:
+        command = FixtureBase(fixture_base).command
+        subprocess.run(command)
+
+
+if __name__ == "__main__":
+    main()
