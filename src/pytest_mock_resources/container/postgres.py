322414635dd494f1fb999bdd96bb229f1b9e8957
Omar Khan <oakhan3@gmail.com>
2019-05-15 11:15:28 -0400

    Support Parallel Testing! Resolves Issue #54

--- src/pytest_mock_resources/container/postgres.py
+++ src/pytest_mock_resources/container/postgres.py
@@ -14,17 +14,22 @@ config = {
 }
 
 
-def get_sqlalchemy_engine(database_name):
-    string = "postgresql+psycopg2://{username}:{password}@{host}:{port}/{database}?sslmode=disable"
-    engine = sqlalchemy.create_engine(
-        string.format(
-            database=database_name,
-            username=config["username"],
-            password=config["password"],
-            host=config["host"],
-            port=config["port"],
-        )
+def get_sqlalchemy_engine(database_name, isolation_level=None):
+    URI_TEMPLATE = (
+        "postgresql+psycopg2://{username}:{password}@{host}:{port}/{database}?sslmode=disable"
     )
+    DB_URI = URI_TEMPLATE.format(
+        database=database_name,
+        username=config["username"],
+        password=config["password"],
+        host=config["host"],
+        port=config["port"],
+    )
+
+    if isolation_level:
+        engine = sqlalchemy.create_engine(DB_URI, isolation_level=isolation_level)
+    else:
+        engine = sqlalchemy.create_engine(DB_URI)
 
     # Verify engine is connected
     engine.connect()
@@ -43,6 +48,26 @@ def check_postgres_fn():
         )
 
 
+def post_postgres_bootup_fn():
+    root_engine = get_sqlalchemy_engine(config["root_database"])
+
+    try:
+        root_engine.execute(
+            """
+            CREATE TABLE IF NOT EXISTS pytest_mock_resource_db(
+                id serial
+            );
+            """
+        )
+    except sqlalchemy.exc.IntegrityError as e:
+        # A race condition may occur during table creation if:
+        #  - another process has already created the table
+        #  - the current process begins creating the table
+        #  - the other process commits the table creation
+        #  - the current process tries to commit the table creation
+        pass
+
+
 _postgres_container = pytest.fixture("session")(
     get_container_fn(
         config["image"],
@@ -53,5 +78,6 @@ _postgres_container = pytest.fixture("session")(
             "POSTGRES_PASSWORD": config["password"],
         },
         check_postgres_fn,
+        post_bootup_fn=post_postgres_bootup_fn,
     )
 )
