42479c0950286abb712f4645df10a967852a05c7
Dan Cardin <ddcardin@gmail.com>
2019-09-25 12:43:47 -0400

    Add support for the "table=" kwarg to accept table models or objects.

--- src/pytest_mock_resources/fixture/database/relational/generic.py
+++ src/pytest_mock_resources/fixture/database/relational/generic.py
@@ -2,9 +2,10 @@ import abc
 
 import six
 from sqlalchemy import MetaData
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.declarative import declarative_base, DeclarativeMeta
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql.ddl import CreateSchema
+from sqlalchemy.sql.schema import Table
 
 from pytest_mock_resources.compat import functools
 
@@ -24,13 +25,13 @@ class Rows(AbstractAction):
     def __init__(self, *rows):
         self.rows = rows
 
-    def run(self, engine, tables):
+    def run(self, engine, tables, default_schema=None):
         rows = self._get_stateless_rows(self.rows)
 
         metadatas = self._get_metadatas(rows)
 
         for metadata in metadatas:
-            _create_ddl(engine, metadata, tables)
+            _create_ddl(engine, metadata, tables, default_schema=default_schema)
 
         self._create_rows(engine, rows)
 
@@ -67,21 +68,20 @@ class Statements(AbstractAction):
     def __init__(self, *statements):
         self.statements = statements
 
-    def run(self, engine, tables):
+    def run(self, engine, tables, default_schema=None):
         for statement in self.statements:
             engine.execute(statement)
 
 
-def _run_actions(engine, ordered_actions, tables=None):
+def _run_actions(engine, ordered_actions, tables=None, default_schema=None):
     BaseType = type(declarative_base())
-
     for action in ordered_actions:
         if isinstance(action, MetaData):
-            _create_ddl(engine, action, tables)
+            _create_ddl(engine, action, tables, default_schema=default_schema)
         elif isinstance(action, BaseType):
-            _create_ddl(engine, action.metadata, tables)
+            _create_ddl(engine, action.metadata, tables, default_schema=default_schema)
         elif isinstance(action, AbstractAction):
-            action.run(engine, tables)
+            action.run(engine, tables, default_schema=default_schema)
         elif callable(action):
             _execute_function(engine, action)
         else:
@@ -90,24 +90,27 @@ def _run_actions(engine, ordered_actions, tables=None):
             )
 
 
-def manage_engine(engine, *args, **kwargs):
+def manage_engine(engine, ordered_actions, tables=None, default_schema=None):
     try:
-        _run_actions(engine, *args, **kwargs)
+        _run_actions(engine, ordered_actions, tables=tables, default_schema=default_schema)
         yield engine
     finally:
         engine.dispose()
 
 
-def _create_ddl(engine, metadata, tables):
-    _create_schemas(engine, metadata)
-    _create_tables(engine, metadata, tables)
+def _create_ddl(engine, metadata, tables=None, default_schema=None):
+    _create_schemas(engine, metadata, default_schema=default_schema)
+    _create_tables(engine, metadata, tables=tables)
 
 
 @functools.lru_cache()
-def _create_schemas(engine, metadata):
-    all_schemas = {table.schema for table in metadata.tables.values() if table.schema} - {"public"}
+def _create_schemas(engine, metadata, default_schema=None):
+    all_schemas = {table.schema for table in metadata.tables.values() if table.schema}
 
     for schema in all_schemas:
+        if default_schema == schema:
+            continue
+
         statement = CreateSchema(schema, quote=True)
         engine.execute(statement)
 
@@ -117,18 +120,26 @@ def _create_tables(engine, metadata, tables):
         metadata.create_all(engine)
         return
 
-    # Make sure to include any implicit or explicit variations of tables in the `public` schema
-    additional_implicit_tables = {table[7:] for table in tables if table.startswith("public.")}
-    additional_explicit_tables = {
-        "public." + table for table in tables if len(table.split(".")) == 1
-    }
+    table_objects = set()
+    for table in tables:
+        if isinstance(table, DeclarativeMeta):
+            table_objects.add(table.__table__)
+        elif isinstance(table, Table):
+            table_objects.add(table)
+        else:
+            table_name = table
 
-    all_tables = set().union(tables, additional_implicit_tables, additional_explicit_tables)
+            if table_name not in metadata.tables:
+                raise ValueError(
+                    'Could not identify table "{}" from: {}'.format(
+                        table_name, ", ".join(sorted(metadata.tables.keys()))
+                    )
+                )
 
-    relevant_table_classes = [
-        table for tablename, table in metadata.tables.items() if tablename in all_tables
-    ]
-    metadata.create_all(engine, tables=relevant_table_classes)
+            table = metadata.tables[table_name]
+            table_objects.add(table)
+
+    metadata.create_all(engine, tables=list(table_objects))
 
 
 def _execute_function(engine, fn):
