5ef6fe6b18881ec2c7e2f16e2c1e046c974a923c
Michael Bukachi <michaelbukachi@gmail.com>
2021-06-17 18:28:41 +0300

    refactor: Change async logic

--- src/pytest_mock_resources/fixture/database/relational/__init__.py
+++ src/pytest_mock_resources/fixture/database/relational/__init__.py
@@ -1,6 +1,5 @@
 # flake8: noqa
 from pytest_mock_resources.fixture.database.relational.generic import Rows, Statements
-from pytest_mock_resources.fixture.database.relational.async_.generic import Rows as AsyncRows, Statements as AsyncStatements
 from pytest_mock_resources.fixture.database.relational.mysql import (
     create_mysql_fixture,
     pmr_mysql_config,
