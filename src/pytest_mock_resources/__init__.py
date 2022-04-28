9e2475a6fee26cba48a91a5f49a26d092280515d
Dan Cardin <ddcardin@gmail.com>
2022-04-28 08:51:07 -0400

    fix: Explicitly export functions intended to be reexported in __all__.

--- src/pytest_mock_resources/__init__.py
+++ src/pytest_mock_resources/__init__.py
@@ -1,4 +1,3 @@
-# flake8: noqa
 from pytest_mock_resources.container import (
     get_container,
     MongoConfig,
@@ -27,9 +26,34 @@ from pytest_mock_resources.fixture.database import (
     Rows,
     Statements,
 )
-from pytest_mock_resources.hooks import (
-    pytest_addoption,
-    pytest_configure,
-    pytest_itemcollected,
-    pytest_sessionfinish,
-)
+from pytest_mock_resources.hooks import pytest_addoption  # flake8: noqa
+from pytest_mock_resources.hooks import pytest_configure  # flake8: noqa
+from pytest_mock_resources.hooks import pytest_itemcollected  # flake8: noqa
+from pytest_mock_resources.hooks import pytest_sessionfinish  # flake8: noqa
+
+__all__ = [
+    'MongoConfig',
+    'MysqlConfig',
+    'PostgresConfig',
+    'RedisConfig',
+    'RedshiftConfig',
+    'Rows',
+    'Statements',
+    'create_mongo_fixture',
+    'create_mysql_fixture',
+    'create_postgres_fixture',
+    'create_redis_fixture',
+    'create_redshift_fixture',
+    'create_sqlite_fixture',
+    'get_container',
+    'pmr_mongo_config',
+    'pmr_mongo_container',
+    'pmr_mysql_config',
+    'pmr_mysql_container',
+    'pmr_postgres_config',
+    'pmr_postgres_container',
+    'pmr_redis_config',
+    'pmr_redis_container',
+    'pmr_redshift_config',
+    'pmr_redshift_container',
+]
--- src/pytest_mock_resources/fixture/database/__init__.py
+++ src/pytest_mock_resources/fixture/database/__init__.py
@@ -1,4 +1,3 @@
-# flake8: noqa
 from pytest_mock_resources.fixture.database.mongo import (
     create_mongo_fixture,
     pmr_mongo_config,
@@ -23,3 +22,24 @@ from pytest_mock_resources.fixture.database.relational import (
     Rows,
     Statements,
 )
+
+__all__ = [
+    'create_mongo_fixture',
+    'create_mysql_fixture',
+    'create_postgres_fixture',
+    'create_redis_fixture',
+    'create_redshift_fixture',
+    'create_sqlite_fixture',
+    'pmr_mongo_config',
+    'pmr_mongo_container',
+    'pmr_mysql_config',
+    'pmr_mysql_container',
+    'pmr_postgres_config',
+    'pmr_postgres_container',
+    'pmr_redis_config',
+    'pmr_redis_container',
+    'pmr_redshift_config',
+    'pmr_redshift_container',
+    'Rows',
+    'Statements',
+]
